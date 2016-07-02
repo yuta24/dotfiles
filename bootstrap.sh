b95bffa3cfa32793f4554e82cd8d4ef773353b5e
yuta24 <yuta24@bivre.com>
2016-07-03 01:48:55 +0900

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git pull origin master;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+doIt;
+
+unset doIt;
