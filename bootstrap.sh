4d762ab7cf992a6ceee6231ceb6c1d2acd5f776b
yuta24 <yuta24@bivre.com>
2021-05-14 23:10:27 +0900

    Add install brew

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,15 @@ function clone() {
 	git clone --depth=1 https://github.com/yuta24/dotfiles.git ~/dotfiles
 }
 
+function install_brew() {
+	which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	which brew >/dev/null 2>&1 && brew doctor
+	which brew >/dev/null 2>&1 && brew update
+}
+
 function main() {
 	clone
+	install_brew
 }
 
 main
