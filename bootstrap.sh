5785d043d0c8c20796a6bcb832b07298ef49fc8b
yuta24 <yuta24@bivre.com>
2021-05-14 23:24:54 +0900

    Fix install brew

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,8 +12,6 @@ function clone() {
 
 function install_brew() {
 	which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	which brew >/dev/null 2>&1 && brew doctor
-	which brew >/dev/null 2>&1 && brew update
 }
 
 function main() {
