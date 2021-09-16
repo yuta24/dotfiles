65f90ae666867c86794e28b7c2f2090e9685897d
yuta24 <yuta24@bivre.com>
2021-09-16 11:21:55 +0900

    Fix install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-function install_brew() {
-    which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    which brew >/dev/null 2>&1 || brew update
-}
+which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which brew >/dev/null 2>&1 || brew update
 
-function main() {
-    install_brew
-}
-
-main
+sh ~/dotfiles/install_tools.sh
