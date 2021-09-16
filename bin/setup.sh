41a01efea617abf1f39465a3d7d45d9e9d86eeef
yuta24 <yuta24@bivre.com>
2021-09-16 14:17:29 +0900

    Setup macOS config

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,3 +2,17 @@
 
 cp -f ~/dotfiles/shell/zsh/.zshrc ~/.zshrc
 source ~/.zshrc
+
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+defaults write com.apple.Finder "AppleShowAllFiles" -bool "true"
+
+killall Finder
+
+defaults write com.apple.dock "autohide" -bool "true"
+defaults write com.apple.dock "orientation" -string "left"
+
+killall Dock
+
+defaults write com.apple.menuextra.clock "FlashDateSeparators" -bool "true"
+
+killall SystemUIServer
